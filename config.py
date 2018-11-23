9c135ce6f4937c0210de360197e24181dd50df90
hsqmlzno1 <hsqmlz@foxmail.com>
2018-11-23 14:07:29 +0800

    HATN

--- config.py
+++ config.py
@@ -12,14 +12,14 @@ tf.flags.DEFINE_float("max_grad_norm", 40.0, "Clip gradients to this norm.")
 tf.flags.DEFINE_float("l2_reg_lambda", 0.005, "L2 regularizaion lambda")
 tf.flags.DEFINE_integer("max_epoch", 100, "Number of epochs to train.")
 tf.flags.DEFINE_integer("evaluation_interval", 1, "Evaluate and print results every x epochs.")
-flags.DEFINE_integer("patience", 5, "number of eval steps to be patient for early stopping")
+tf.flags.DEFINE_integer("patience", 5, "number of eval steps to be patient for early stopping")
 
 # ************** network configuration **************
 tf.flags.DEFINE_integer("memory_size", 20, "Maximum memory size.")
 tf.flags.DEFINE_integer("sent_size", 25, "Maximum sentence size.")
 tf.flags.DEFINE_integer("embed_size", 300, "Embedding size.")
 tf.flags.DEFINE_integer("hidden_size", 300, "Hidden size.")
-tf.flags.DEFINE_integer("hops", 1, "Number of hops in the for word/sentence attention layer.")
+tf.flags.DEFINE_integer("hops", 1, "Number of hops for the word/sentence attention layer.")
 
 # ************** source configuration **************
 tf.flags.DEFINE_string("data_path", "./data/", "Data path")
